90aece2c4faf26893c0fc71671700a1625fd37bd
shingo.hatashi <hatashi0972015@icloud.com>
2020-09-08 07:01:15 +0900

    Initial Commit

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by 幡司真吾 on 2020/09/08.
+//  Copyright © 2020 shingo.hatashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
